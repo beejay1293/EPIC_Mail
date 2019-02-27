e040fbfdae0f059c8feb076e7cbd49d45e5ff441
beejay1293 <mattimobolaji@gmail.com>
2019-02-27 17:51:07 +0100

    feat(user reset password): User can now reset password
    this feature allows user to reset thier password successfully
    
    [Delievers #164158774]

--- UI/js/dashboard.js
+++ UI/js/dashboard.js
@@ -10,19 +10,32 @@ const draftBody = document.querySelector(".draft__body");
 const starredBody = document.querySelector(".starred__body");
 const topNav = document.querySelector(".top__nav");
 const readEmail = document.querySelector(".read__email");
+const passwordResetOverlay = document.querySelector(".password__reset_overlay");
+const topNavInbox = document.querySelector(".top_nav_inbox__btn");
+const topNavSent = document.querySelector(".top_nav_sent__btn");
+const topNavDraft = document.querySelector(".top_nav_draft__btn");
+const topNavStarred = document.querySelector(".top_nav_starred__btn");
 
 document.querySelector(".time__created").textContent = new Date().toUTCString();
 
 document.addEventListener("click", e => {
+  if (e.target.className == "reset__password") {
+    passwordResetOverlay.style.display = "block";
+  }
+
+  if (e.target.className == "clear__overlay") {
+    passwordResetOverlay.style.display = "none";
+  }
+
   if (e.target.parentNode.className == "draft__message") {
     overlay.style.display = "block";
   }
 
   if (
-    e.target.className == "top_nav_inbox__btn" ||
-    e.target.className == "top_nav_sent__btn" ||
-    e.target.className == "top_nav_draft__btn" ||
-    e.target.className == "top_nav_starred__btn"
+    e.target.classList[0] == "top_nav_inbox__btn" ||
+    e.target.classList[0] == "top_nav_sent__btn" ||
+    e.target.classList[0] == "top_nav_draft__btn" ||
+    e.target.classList[0] == "top_nav_starred__btn"
   ) {
     topNav.classList.toggle("top__navs");
     messageBody.classList.toggle("main__body__height");
@@ -40,6 +53,7 @@ document.addEventListener("click", e => {
   if (e.target.className == "clear__overlay") {
     overlay.style.display = "none";
   }
+
   if (
     e.target.classList[0] == "inbox__btn" ||
     e.target.classList[0] == "top_nav_inbox__btn"
@@ -48,6 +62,11 @@ document.addEventListener("click", e => {
     sentButton.classList.remove("active");
     starredButton.classList.remove("active");
     inboxButton.classList.add("active");
+
+    topNavSent.classList.remove("actives");
+    topNavDraft.classList.remove("actives");
+    topNavStarred.classList.remove("actives");
+    topNavInbox.classList.add("actives");
 
     inboxBody.style.display = "block";
     draftBody.style.display = "none";
@@ -65,6 +84,11 @@ document.addEventListener("click", e => {
     starredButton.classList.remove("active");
     draftButton.classList.add("active");
 
+    topNavSent.classList.remove("actives");
+    topNavDraft.classList.add("actives");
+    topNavStarred.classList.remove("actives");
+    topNavInbox.classList.remove("actives");
+
     draftBody.style.display = "block";
     inboxBody.style.display = "none";
     sentBody.style.display = "none";
@@ -80,6 +104,11 @@ document.addEventListener("click", e => {
     draftButton.classList.remove("active");
     starredButton.classList.remove("active");
     sentButton.classList.add("active");
+
+    topNavSent.classList.add("actives");
+    topNavDraft.classList.remove("actives");
+    topNavStarred.classList.remove("actives");
+    topNavInbox.classList.remove("actives");
 
     sentBody.style.display = "block";
     inboxBody.style.display = "none";
@@ -97,13 +126,17 @@ document.addEventListener("click", e => {
     sentButton.classList.remove("active");
     starredButton.classList.add("active");
 
+    topNavSent.classList.remove("actives");
+    topNavDraft.classList.remove("actives");
+    topNavStarred.classList.add("actives");
+    topNavInbox.classList.remove("actives");
+
     starredBody.style.display = "block";
     draftBody.style.display = "none";
     sentBody.style.display = "none";
     inboxBody.style.display = "none";
     readEmail.style.display = "none";
   }
-  console.log(e.target.classList[0]);
 
   if (
     e.target.parentNode.className == "inbox__message" ||
@@ -119,4 +152,5 @@ document.addEventListener("click", e => {
 
 window.onload = () => {
   inboxButton.classList.add("active");
+  topNavInbox.classList.add("actives");
 };
