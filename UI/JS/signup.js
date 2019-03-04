f9f5c592aeab48a1e162569b28009a9ee857fdb9
beejay1293 <mattimobolaji@gmail.com>
2019-03-04 11:42:02 +0100

    fix develop js bug

---
+++ UI/JS/signup.js
@@ -0,0 +1,54 @@
+const registerbtn = document.querySelector(".signupbtn");
+const loginbtn = document.querySelector(".signinbtn");
+const register = document.querySelector(".register");
+const login = document.querySelector(".login");
+const login_wrapper = document.querySelector(".login__wrapper");
+const recoverPassword = document.querySelector(".recover__password");
+const forgotPassword = document.querySelector(".forgot__password");
+const loginButton = document.querySelector(".login__btn");
+
+//add a click event listener to registerbtn
+registerbtn.addEventListener("click", () => {
+  //display register form
+  register.style.display = "block";
+
+  //hide login form
+  login.style.display = "none";
+  recoverPassword.style.display = "none";
+
+  login_wrapper.classList.add("pad");
+});
+
+//add a click event listener to loginbtn
+loginbtn.addEventListener("click", () => {
+  //display login form
+  login.style.display = "block";
+
+  //hide register form
+  register.style.display = "none";
+  recoverPassword.style.display = "none";
+
+  login_wrapper.classList.remove("pad");
+});
+
+forgotPassword.addEventListener("click", () => {
+  //display login form
+  login.style.display = "none";
+
+  //hide register form
+  register.style.display = "none";
+  recoverPassword.style.display = "block";
+
+  login_wrapper.classList.remove("pad");
+});
+
+loginButton.addEventListener("click", () => {
+  //display login form
+  login.style.display = "block";
+
+  //hide register form
+  register.style.display = "none";
+  recoverPassword.style.display = "none";
+
+  login_wrapper.classList.remove("pad");
+});
