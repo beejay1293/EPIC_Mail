9586768d2cf47b3658d3f514e14668c1ff442edf
beejay1293 <mattimobolaji@gmail.com>
2019-03-06 02:05:48 +0100

    feat(user can get all received messages): User can get all received message
    this feature allows users to get all received messages
    
    [Delievers #164408920]

--- server/routes/api/message.js
+++ server/routes/api/message.js
@@ -6,5 +6,8 @@ const router = express.Router();
 // create a message route
 router.post('/messages', MessageController.createMessage);
 
+// get all received messages route
+router.get('/messages', MessageController.GetAllReceivedMessages);
+
 // expose router
 export default router;
