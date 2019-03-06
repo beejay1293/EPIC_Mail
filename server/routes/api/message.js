d88321611c834613aebdfec0341ed91d97a2eaf3
beejay1293 <mattimobolaji@gmail.com>
2019-03-06 15:44:33 +0100

    feat(get all sent messages): get all sent messages
    this endpoint enables user get all sent messages
    
    [Delievers #164430844]

--- server/routes/api/message.js
+++ server/routes/api/message.js
@@ -9,7 +9,11 @@ router.post('/messages', MessageController.createMessage);
 // get all received messages route
 router.get('/messages', MessageController.GetAllReceivedMessages);
 
+// get all unread messages enpoint
 router.get('/messages/unread', MessageController.GetAllUnreadReceivedMessages);
+
+// get all sent messages endpoint
+router.get('/messages/sent', MessageController.GetAllSentMessages);
 
 // expose router
 export default router;
