422c6842ad6a251adcc7d5f083f70de418241ab3
beejay1293 <mattimobolaji@gmail.com>
2019-03-20 12:58:49 +0100

    Updates

--- JS/signup.js
+++ JS/signup.js
@@ -1,54 +1,54 @@
-const registerbtn = document.querySelector(".signupbtn");
-const loginbtn = document.querySelector(".signinbtn");
-const register = document.querySelector(".register");
-const login = document.querySelector(".login");
-const login_wrapper = document.querySelector(".login__wrapper");
-const recoverPassword = document.querySelector(".recover__password");
-const forgotPassword = document.querySelector(".forgot__password");
-const loginButton = document.querySelector(".login__btn");
+const registerbtn = document.querySelector('.signupbtn');
+const loginbtn = document.querySelector('.signinbtn');
+const register = document.querySelector('.register');
+const login = document.querySelector('.login');
+const loginWrapper = document.querySelector('.login__wrapper');
+const recoverPassword = document.querySelector('.recover__password');
+const forgotPassword = document.querySelector('.forgot__password');
+const loginButton = document.querySelector('.login__btn');
 
-//add a click event listener to registerbtn
-registerbtn.addEventListener("click", () => {
-  //display register form
-  register.style.display = "block";
+// add a click event listener to registerbtn
+registerbtn.addEventListener('click', () => {
+  // display register form
+  register.style.display = 'block';
 
-  //hide login form
-  login.style.display = "none";
-  recoverPassword.style.display = "none";
+  // hide login form
+  login.style.display = 'none';
+  recoverPassword.style.display = 'none';
 
-  login_wrapper.classList.add("pad");
+  loginWrapper.classList.add('pad');
 });
 
-//add a click event listener to loginbtn
-loginbtn.addEventListener("click", () => {
-  //display login form
-  login.style.display = "block";
+// add a click event listener to loginbtn
+loginbtn.addEventListener('click', () => {
+  // display login form
+  login.style.display = 'block';
 
-  //hide register form
-  register.style.display = "none";
-  recoverPassword.style.display = "none";
+  // hide register form
+  register.style.display = 'none';
+  recoverPassword.style.display = 'none';
 
-  login_wrapper.classList.remove("pad");
+  loginWrapper.classList.remove('pad');
 });
 
-forgotPassword.addEventListener("click", () => {
-  //display login form
-  login.style.display = "none";
+forgotPassword.addEventListener('click', () => {
+  // display login form
+  login.style.display = 'none';
 
-  //hide register form
-  register.style.display = "none";
-  recoverPassword.style.display = "block";
+  // hide register form
+  register.style.display = 'none';
+  recoverPassword.style.display = 'block';
 
-  login_wrapper.classList.remove("pad");
+  loginWrapper.classList.remove('pad');
 });
 
-loginButton.addEventListener("click", () => {
-  //display login form
-  login.style.display = "block";
+loginButton.addEventListener('click', () => {
+  // display login form
+  login.style.display = 'block';
 
-  //hide register form
-  register.style.display = "none";
-  recoverPassword.style.display = "none";
+  // hide register form
+  register.style.display = 'none';
+  recoverPassword.style.display = 'none';
 
-  login_wrapper.classList.remove("pad");
+  loginWrapper.classList.remove('pad');
 });
